f59c9377b5e2b101dd0a585ccbe7a80868380ef0
wurenjie <wurenjie@fosun.com>
2019-02-24 15:35:31 -0800

    initial commit

---
+++ include/Edge.h
@@ -0,0 +1,38 @@
+//
+// Created by Renjie Wu on 2019-02-24.
+//
+
+#ifndef PIPELINE_GRAPH_EDGE_H
+#define PIPELINE_GRAPH_EDGE_H
+
+#include <utility>
+class Node;
+
+class Edge {
+    typedef std::pair<std::shared_ptr<Node>,std::shared_ptr<Node>> pipe_type;
+    int edge_id_;
+    pipe_type end_points_;
+public:
+    Edge(const int id, const Node& n_1, const Node& n_2):edge_id_(id){
+        set_end_points(n_1,n_2);
+    }
+    //get/set functions
+    double get_edge_id() const {
+        return  edge_id_;
+    }
+    pipe_type get_end_points() const {
+        return end_points_;
+    }
+
+    void set_edge_id(const int id){
+        edge_id_ = id;
+    }
+    void set_end_points(const Node& n_1, const Node& n_2){
+        end_points_ = std::make_pair(std::make_shared<Node>(n_1),std::make_shared<Node>(n_2));
+    }
+
+
+};
+
+
+#endif //PIPELINE_GRAPH_EDGE_H
---
+++ include/Node.h
@@ -0,0 +1,34 @@
+//
+// Created by Renjie Wu on 2019-02-24.
+//
+
+#ifndef PIPELINE_GRAPH_NODE_H
+#define PIPELINE_GRAPH_NODE_H
+
+#include <utility>
+
+class Node {
+int node_id_;
+std::pair<double ,double > node_coord_;
+public:
+    // constructor
+    Node(const int node_id, const double coord_x, const double coord_y):
+    node_id_(node_id), node_coord_(coord_x,coord_y){};
+    // get functions
+    long get_node_id() const {
+        return  node_id_;
+    }
+    std::pair<double ,double > get_coordinate() const {
+        return node_coord_;
+    };
+    // set functions
+    void set_node_id(const int id){
+        node_id_ = id;
+    }
+    void set_coordinate(const double coord_x,const double coord_y){
+        node_coord_ = std::make_pair(coord_x,coord_y);
+    }
+};
+
+
+#endif //PIPELINE_GRAPH_NODE_H
